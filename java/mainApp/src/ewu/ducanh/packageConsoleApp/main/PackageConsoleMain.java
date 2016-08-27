2c53473b13aeb8ffb3630d435e4fd582cd466298
atran14 <atran14@eagles.ewu.edu>
2016-08-28 00:20:16 +0700

    Update existing files

---
+++ java/mainApp/src/ewu/ducanh/packageConsoleApp/main/PackageConsoleMain.java
@@ -0,0 +1,7 @@
+package ewu.ducanh.packageConsoleApp.main;
+
+/**
+ * Created by DucAnh on 8/28/16.
+ */
+public class PackageConsoleMain {
+}
--- java/mainApp/src/ewu/ducanh/packageConsoleApp/main/classes/ConsoleAppMain.java
+++
@@ -1,7 +0,0 @@
-package ewu.ducanh.packageConsoleApp.main.classes;
-
-/**
- * Created by DucAnh on 8/28/16.
- */
-public class ConsoleAppMain {
-}
